99b563c928204f6fe51735e6a0252d7f043c18ec
vladislav149 <vladislav149@mail.ru>
2022-02-04 22:18:49 +0300

    added 25ArrayMetod.js

---
+++ js/25ArrayMetods.js
@@ -0,0 +1,28 @@
+// 1 task
+/* function camelize(str) {
+  let arr = str.split('')
+  console.log(arr);
+  let result = arr.findIndex(item => item == '-')
+  console.log(result);
+  arr.splice(result, 1);
+  console.log(arr);
+  arr.splice(result, 1, arr[result].toUpperCase());
+  console.log(arr);
+  arr = arr.join('');
+  console.log(arr);
+}
+
+camelize('hello-hi') */
+
+
+//2 task
+
+/* let arr = [5, 3, 8, 1];
+
+function filterRange(array, a, b) {
+  return array.filter(item => item >= a && item <= b);
+}
+
+let filtered = filterRange(arr, 1, 4);
+console.log(filtered);
+console.log(arr); */
